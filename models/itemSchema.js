0b5882aa486f9f4abf0d6a929a53f6bedca4c2cf
RufioYrael <4.3.1e.1987@gmail.com>
2023-08-07 18:55:11 -0400

    first commit

---
+++ models/itemSchema.js
@@ -0,0 +1,14 @@
+const item = require('./item')
+
+const Schema = require('mongoose').Schema
+
+const itemSchema = new Scheam ({
+    name: { type: String, required: true },
+    emoji: String,
+    category: { type: Schema.Types.ObjectId, ref: 'Category' },
+    price: { type: Number, required: true, default: 0 }
+} , {
+    timestampes: true
+})
+
+module.exports = itemSchema
