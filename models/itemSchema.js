41ca07bcc8c7c6f2c4dbf94a51b4399eda03db2c
TJCole03 <tjc6254@gmail.com>
2023-08-07 21:17:23 -0400

    Seed file added

--- models/itemSchema.js
+++ models/itemSchema.js
@@ -2,7 +2,7 @@ const item = require('./item')
 
 const Schema = require('mongoose').Schema
 
-const itemSchema = new Scheam ({
+const itemSchema = new Schema ({
     name: { type: String, required: true },
     emoji: String,
     category: { type: Schema.Types.ObjectId, ref: 'Category' },
