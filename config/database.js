af56ea3c54410321c3a710f5945c0c64ba93be2b
TJCole03 <tjc6254@gmail.com>
2023-08-07 21:35:47 -0400

    test

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ mongoose.connect(process.env.MONGO_URI)
 const db = mongoose.connection
 
 db.on('connected', () => {
-    console.log(`Connected to ${db.name} at ${db.host}:${db.port}`)
+    console.log(`Connected to ${db.name} at ${db.host}:${db.port} whoop whoop!`)
 })
 
 module.exports = mongoose
